2710d135d0041179492306a9c5e31aed55cbe75f
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-27 15:03:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-//your JS code here. If required.
+  const username = document.querySelector("#username");
+      const password = document.querySelector("#password");
+      const check    = document.querySelector('input[type="checkbox"]');
+      const subBtn   = document.querySelector("#submit");
+      const existingBtn = document.querySelector("#exisiting");
+      console.log("check1");
+     
+        const userData = localStorage.getItem("users");
+        if (!userData) {
+            localStorage.removeItem('users')
+            existingBtn.style.visibility="hidden"
+        }
+      
+      subBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("check2");
+
+        if (check.checked == false) {
+          alert(`Logged in as ${username.value}`);
+        } else {
+          const data = {
+            userName: username.value,
+            password: password.value,
+            checkbox: check.checked,
+          };
+          console.log("check3");
+
+          localStorage.setItem("users", JSON.stringify(data));
+            existingBtn.style.visibility="hidden"
+        }
+        alert("User data has been saved.");
+      });
+
+      existingBtn.addEventListener('click',(e)=>{
+        e.preventDefault()
+        const data = localStorage.getItem('users');
+        const parsedData = JSON.parse(data);
+       alert(`Logged in as ${parsedData.userName}`)
+
+      })
